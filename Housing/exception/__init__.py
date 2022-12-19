787513c97a64b8afec065d1cd7943dc4784de917
K-Sricharan <sricharan.karingula@gmail.com>
2022-12-19 12:50:04 +0530

    done

--- Housing/exception/__init__.py
+++ Housing/exception/__init__.py
@@ -5,6 +5,8 @@ class Housingexception(Exception):
     
     def __init__(self, error_message: Exception, error_details: sys):
         super().__init__(error_message)
+        self.error_message=Housingexception.get_detailed_error_message(error_message=error_message,
+                                                        error_details=error_details)
 
         
 
@@ -26,3 +28,11 @@ class Housingexception(Exception):
         error_message = f"Error occured in script: [{file_name}] at line number: [{line_number}] error_message: [{error_message}]"
         
         return error_message
+
+
+    def __str__(self):
+        return self.error_message
+
+
+    def __repr__(self) -> str:
+        return Housingexception.__name__.str()
--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask
+import sys
 from Housing.logger import logging
+from Housing.exception import Housingexception
 
 app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    logging.info("we are testing logging module")
+    try:
+        raise Exception("we are testing custom error message")
+    except Exception as e:
+        housing = Housingexception(e,sys)
+        logging.info(housing.error_message)
+        logging.info("we are testing logging module")
     return "Starting Machine learning Project"
 
 
